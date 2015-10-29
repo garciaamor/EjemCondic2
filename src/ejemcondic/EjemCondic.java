8f4b2f988b28a9c084e0b812246b83150fd7f6a1
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-10-29 12:02:03 +0100

    Ejemplo Condicionales comparacion numeros

---
+++ src/ejemcondic/EjemCondic.java
@@ -0,0 +1,12 @@
+package ejemcondic;
+
+public class EjemCondic {
+
+    public static void main(String[] args) {
+    int numero1=Compara.pedirNumEnt();
+    int numero2=Compara.pedirNumEnt();
+    Compara.comparar(numero1, numero2);
+    }
+        
+    
+}
